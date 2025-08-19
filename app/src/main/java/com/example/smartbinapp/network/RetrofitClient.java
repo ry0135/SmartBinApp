b29c41439952619dc5f422e26ec25d98a7c8d26d
ry0135 <namdpde170457@fpt.edu.vn>
2025-08-19 23:15:27 +0700

    config

---
+++ app/src/main/java/com/example/smartbinapp/network/RetrofitClient.java
@@ -0,0 +1,35 @@
+package com.example.smartbinapp.network;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+
+
+    private static Retrofit retrofit;
+
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            // Tạo logger
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY); // Có thể dùng BASIC nếu không cần log body
+
+            // Thêm logger vào OkHttpClient
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(logging)
+                    .build();
+
+            // Khởi tạo Retrofit
+            retrofit = new Retrofit.Builder()
+                    .baseUrl("http://10.0.2.2:8080/TestSmartBin_war/") // Đúng với máy thật hoặc emulator
+                    .client(client) // sử dụng client có log
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+
+
+}
