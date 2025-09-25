c7c43d0077156dac87b74241cea978c89bc2370b
ry0135 <namdpde170457@fpt.edu.vn>
2025-09-25 20:06:23 +0700

    commit new

---
+++ app/src/main/java/com/example/smartbinapp/model/Task.java
@@ -0,0 +1,64 @@
+package com.example.smartbinapp.model;
+
+
+import java.util.Date;
+
+public class Task {
+    private int taskID;
+    private String taskType;
+    private int priority;
+    private String status;
+    private String notes;
+    private String batchId;
+    private Bin bin;
+
+    private Date createdAt;   // để Gson parse JSON ISO8601
+    private Date completedAt;
+
+    private Account assignedTo; // thêm vào để khớp JSON backend
+
+    private String beforeImage;
+    private String afterImage;
+
+    // Getter & Setter
+    public int getTaskID() { return taskID; }
+    public void setTaskID(int taskID) { this.taskID = taskID; }
+
+    public String getTaskType() { return taskType; }
+    public void setTaskType(String taskType) { this.taskType = taskType; }
+
+    public int getPriority() { return priority; }
+    public void setPriority(int priority) { this.priority = priority; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+
+    public String getNotes() { return notes; }
+    public void setNotes(String notes) { this.notes = notes; }
+
+    public String getBatchId() { return batchId; }
+    public void setBatchId(String batchId) { this.batchId = batchId; }
+
+    public Bin getBin() { return bin; }
+    public void setBin(Bin bin) { this.bin = bin; }
+
+    public Date getCreatedAt() { return createdAt; }
+    public void setCreatedAt(Date createdAt) { this.createdAt = createdAt; }
+
+    public Date getCompletedAt() { return completedAt; }
+    public void setCompletedAt(Date completedAt) { this.completedAt = completedAt; }
+
+    public String getBeforeImage() { return beforeImage; }
+    public void setBeforeImage(String beforeImage) { this.beforeImage = beforeImage; }
+
+    public String getAfterImage() { return afterImage; }
+    public void setAfterImage(String afterImage) { this.afterImage = afterImage; }
+
+    public Account getAssignedTo() {
+        return assignedTo;
+    }
+
+    public void setAssignedTo(Account assignedTo) {
+        this.assignedTo = assignedTo;
+    }
+}
